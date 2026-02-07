a8f88e43b3aeb2b8e91b88c53605813b8b615ebc
S.Khalezov <energet666@gmail.com>
2026-02-08 01:44:24 +0500

    Refactor: improve code structure, reliability, and user experience

--- createConfigsIfNotExist.go
+++ createConfigsIfNotExist.go
@@ -2,23 +2,25 @@ package main
 
 import "os"
 
-func createConfigsIfNotExist() error {
+func createConfigsIfNotExist() (bool, error) {
 	entries, err := os.ReadDir("templates")
 	if err != nil {
-		return err
+		return false, err
 	}
+	created := false
 	for _, f := range entries {
 		_, err := os.Stat(".private/" + f.Name())
 		if err != nil {
 			if os.IsNotExist(err) {
 				err := copyFile("templates/"+f.Name(), ".private/"+f.Name())
 				if err != nil {
-					return err
+					return false, err
 				}
+				created = true
 			} else {
-				return err
+				return false, err
 			}
 		}
 	}
-	return nil
+	return created, nil
 }
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"log"
+	"log/slog"
 	"os"
+	"os/exec"
+	"os/signal"
 	"slices"
+	"syscall"
 	"time"
 
 	ffmpeg "github.com/energet666/goffmpeg"
@@ -18,106 +22,174 @@ type Playlist struct {
 	Translate   bool   `json:"translate"`
 }
 
+const (
+	configPath     = ".private/config.json"
+	downloadedPath = ".private/downloaded.json"
+	ytDlpConfPath  = ".private/yt-dlp.conf"
+)
+
 func main() {
-	err := createConfigsIfNotExist()
-	if err != nil {
-		panic(err)
+	// Настраиваем красивый текстовый логгер
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	slog.SetDefault(logger)
+
+	if err := checkDependencies(); err != nil {
+		slog.Error("Dependencies check failed", "error", err)
+		os.Exit(1)
 	}
 
-	playlists := new([]Playlist)
-	playlistsJSON, err := os.ReadFile(".private/config.json")
+	created, err := createConfigsIfNotExist()
 	if err != nil {
-		panic(err)
+		slog.Error("Critical error creating configs", "error", err)
+		os.Exit(1)
 	}
-	err = json.Unmarshal(playlistsJSON, playlists)
-	if err != nil {
-		panic(err)
-	}
-
-	downloadedVideos := new([]string)
-	downloadedJSON, err := os.ReadFile(".private/downloaded.json")
-	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal(downloadedJSON, downloadedVideos)
-	if err != nil {
-		panic(err)
+	if created {
+		slog.Info("First run: configuration files created in .private/ directory")
+		slog.Info("Please edit .private/config.json and .private/yt-dlp.conf, then restart the service")
+		os.Exit(0)
 	}
 
-	yt := goytdlp.NewYtDlp(".private/yt-dlp.conf")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	slog.Info("Service started", "interval", "1 minute")
 
 	for {
-		downloadedFileNeedUpdate := false
-
-		for _, playlist := range *playlists {
-			if playlist.PlaylistURL == "" {
-				log.Println("PlaylistURL is empty")
-				continue
-			}
-
-			videos, err := yt.ScanPlaylist(playlist.PlaylistURL)
-			if err != nil {
-				log.Println("error scanning playlist:", err)
-				time.Sleep(time.Second * 10)
-				continue
-			}
-
-			for _, video := range videos {
-				if slices.Contains(*downloadedVideos, video.Id) {
-					continue
-				}
-				fmt.Println("New video:", video.Title)
-				fmt.Println("\t", video.Url)
-				fmt.Println("\t", video.Id)
-
-				err := yt.Download(video.Url)
-				if err != nil {
-					log.Println("downloading video error:", err)
-					continue
-				}
-
-				if playlist.Translate {
-					//yt-dlp обрабатывает название видео чтобы не было проблем с файловой системой
-					//приходится у yt-dlp запрашивать получившееся имя файла
-					filename, err := yt.GetFilename(video.Url)
-					if err != nil {
-						log.Println("[yt-dlp] get filename error:", err)
-						continue
-					}
-					err = votcli.Download(video.Url, ".", filename+".mp3")
-					if err != nil {
-						log.Println("[vot-cli]", err)
-						continue
-					}
-
-					if _, err := os.Stat(filename + ".mp3"); err == nil {
-						log.Println(filename+".mp3", "is exist")
-						err = ffmpeg.AddAudio(filename, filename+".mp3", filename+".[VOT-CLI].mp4")
-						if err != nil {
-							log.Println("[ffmpeg]", err)
-							continue
-						}
-					} else {
-						log.Println(filename+".mp3", "is not exist")
-					}
-				}
-
-				*downloadedVideos = append(*downloadedVideos, video.Id)
-				downloadedFileNeedUpdate = true
-			}
+		if err := runScanCycle(ctx); err != nil {
+			slog.Error("Error in scan cycle", "error", err)
 		}
 
-		if downloadedFileNeedUpdate {
-			data, err := json.Marshal(downloadedVideos)
-			if err != nil {
-				log.Println("marshaling downloaded list error:", err)
-			}
-			err = os.WriteFile(".private/downloaded.json", data, 0644)
-			if err != nil {
-				log.Println("writing downloaded list to file error:", err)
-			}
+		select {
+		case <-time.After(time.Minute):
+		case <-ctx.Done():
+			slog.Info("Shutting down gracefully...")
+			return
 		}
-
-		time.Sleep(time.Second * 60)
 	}
+}
+
+func runScanCycle(ctx context.Context) error {
+	var playlists []Playlist
+	if err := loadJSON(configPath, &playlists); err != nil {
+		return err
+	}
+
+	var downloadedVideos []string
+	if err := loadJSON(downloadedPath, &downloadedVideos); err != nil {
+		return err
+	}
+
+	yt := goytdlp.NewYtDlp(ytDlpConfPath)
+
+	for _, playlist := range playlists {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+
+		if playlist.PlaylistURL == "" {
+			slog.Warn("Skipping empty PlaylistURL")
+			continue
+		}
+
+		videos, err := yt.ScanPlaylist(playlist.PlaylistURL)
+		if err != nil {
+			slog.Error("Error scanning playlist", "url", playlist.PlaylistURL, "error", err)
+			continue
+		}
+
+		for _, video := range videos {
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
+
+			if slices.Contains(downloadedVideos, video.Id) {
+				continue
+			}
+
+			slog.Info("Processing new video",
+				"title", video.Title,
+				"id", video.Id,
+				"translate", playlist.Translate)
+
+			if err := handleVideo(yt, video, playlist.Translate); err != nil {
+				slog.Error("Error processing video", "id", video.Id, "error", err)
+				continue
+			}
+
+			downloadedVideos = append(downloadedVideos, video.Id)
+			if err := saveJSON(downloadedPath, downloadedVideos); err != nil {
+				slog.Error("Error saving downloaded list", "error", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func handleVideo(yt *goytdlp.YtDlp, video goytdlp.Video, translate bool) error {
+	err := yt.Download(video.Url)
+	if err != nil {
+		return err
+	}
+
+	if !translate {
+		return nil
+	}
+
+	filename, err := yt.GetFilename(video.Url)
+	if err != nil {
+		return err
+	}
+
+	audioFile := filename + ".mp3"
+	translatedVideo := filename + ".[VOT-CLI].mp4"
+
+	slog.Info("Downloading translation", "filename", filename)
+	err = votcli.Download(video.Url, ".", audioFile)
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(audioFile); err == nil {
+		slog.Info("Muxing audio with ffmpeg", "output", translatedVideo)
+		err = ffmpeg.AddAudio(filename, audioFile, translatedVideo)
+		if err != nil {
+			return err
+		}
+
+		slog.Info("Cleaning up temporary files", "files", []string{audioFile, filename})
+		os.Remove(audioFile)
+		os.Remove(filename)
+	} else {
+		return fmt.Errorf("audio file not found: %s", audioFile)
+	}
+
+	return nil
+}
+
+func loadJSON(path string, v interface{}) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, v)
+}
+
+func saveJSON(path string, v interface{}) error {
+	data, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
+
+func checkDependencies() error {
+	deps := []string{"yt-dlp", "ffmpeg", "vot-cli"}
+	for _, dep := range deps {
+		_, err := exec.LookPath(dep)
+		if err != nil {
+			return fmt.Errorf("required dependency not found: %s (please install it)", dep)
+		}
+	}
+	return nil
 }
