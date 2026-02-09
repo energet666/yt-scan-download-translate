42374d7a4cbe865e18562e89ec26ce51ff2d4b34
S.Khalezov <energet666@gmail.com>
2026-02-09 21:09:02 +0500

    Extract download command args to slice

--- votclilive/download.go
+++ votclilive/download.go
@@ -6,7 +6,12 @@ import (
 )
 
 func Download(url string, path string, filename string) error {
-	cmd := exec.Command("vot-cli-live", "--output="+path, "--output-file="+filename, url)
+	args := []string{
+		"--output=" + path,
+		"--output-file=" + filename,
+		url,
+	}
+	cmd := exec.Command("vot-cli-live", args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
